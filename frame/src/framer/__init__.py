05d6db0dcab1619e7954da6a9cd29bf7dfb42353
Johnny Dunn <johnnyfived@protonmail.com>
2024-10-15 17:31:34 -0700

    chore: Add __init__.py to make framer directory a package

--- frame/src/framer/__init__.py
+++ frame/src/framer/__init__.py
@@ -3,3 +3,4 @@ from .framer_factory import FramerFactory
 from .config import FramerConfig
 
 __all__ = ["Framer", "FramerFactory", "FramerConfig"]
+# This file makes the framer directory a package.
