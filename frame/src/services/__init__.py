58345c52208347aa3ce510ce13477fb387567de6
Johnny Dunn <johnnyfived@protonmail.com>
2024-10-18 02:25:52 -0700

    resolve circular import issue in frame/src/services/__init__.py

--- frame/src/services/__init__.py
+++ frame/src/services/__init__.py
@@ -4,9 +4,7 @@ from .context.local_context_service import LocalContext
 from .context.shared_context_service import SharedContext
 from .eq.main import EQService
 
-# Import ExecutionContext at the end to avoid circular imports
-from .context.execution_context_service import ExecutionContext
-
 # Create a function to get ExecutionContext to break circular imports
 def get_execution_context():
+    from .context.execution_context_service import ExecutionContext
     return ExecutionContext
