4f8d0d614e010820ea39b961eea65af107883a79
Johnny Dunn <johnnyfived@protonmail.com>
2024-10-18 03:29:03 -0700

    restructure services to avoid circular imports

--- frame/src/services/__init__.py
+++ frame/src/services/__init__.py
@@ -1,19 +1,17 @@
 from .llm.main import LLMService
 from .eq.main import EQService
+from .service_locator import (
+    get_local_context,
+    get_shared_context,
+    get_execution_context,
+    get_memory_service
+)
 
-# Create functions to get services to break circular imports
-def get_local_context():
-    from .context.local_context_service import LocalContext
-    return LocalContext
-
-def get_shared_context():
-    from .context.shared_context_service import SharedContext
-    return SharedContext
-
-def get_execution_context():
-    from .context.execution_context_service import ExecutionContext
-    return ExecutionContext
-
-def get_memory_service():
-    from .memory.main import MemoryService
-    return MemoryService
+__all__ = [
+    'LLMService',
+    'EQService',
+    'get_local_context',
+    'get_shared_context',
+    'get_execution_context',
+    'get_memory_service'
+]
