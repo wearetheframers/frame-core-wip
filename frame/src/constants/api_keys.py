9f78222f050ff90d42111a1393b79fa5744bd1e3
Johnny Dunn <johnnyfived@protonmail.com>
2024-10-18 04:49:17 -0700

    fix: add missing MEM0_API_KEY

--- frame/src/constants/api_keys.py
+++ frame/src/constants/api_keys.py
@@ -10,3 +10,5 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY", "")
 HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY", "")
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY", "")
+
+MEM0_API_KEY = os.getenv("MEM0_API_KEY", "")
